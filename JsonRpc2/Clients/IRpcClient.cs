f982c3fbeeefa057a0fa91fdd2c6dd015e156187
Matthew Burkard <matthewburkard@protonmail.com>
2021-08-21 12:07:31 -0400

    feat: add rpc exceptions
    The appropriate RPC error is thrown based on error code.

--- JsonRpc2/Clients/IRpcClient.cs
+++ JsonRpc2/Clients/IRpcClient.cs
@@ -1,4 +1,5 @@
 using System;
+using JsonRpc2.Exceptions;
 using JsonRpc2.JsonRpcObjects;
 using Newtonsoft.Json;
 
@@ -6,6 +7,12 @@ namespace JsonRpc2.Clients
 {
     public interface IRpcClient
     {
+        const int ParseError =-32700;
+        const int InvalidRequest = -32600;
+        const int MethodNotFound =-32601;
+        const int InvalidParams =-32602;
+        const int InternalError =-32603;
+
         protected static string GenId()
         {
             return Guid.NewGuid().ToString();
@@ -16,7 +23,15 @@ namespace JsonRpc2.Clients
             var resp = JsonConvert.DeserializeObject<RpcResponse>(data);
             if (resp is {Error: { }})
             {
-                throw new Exception("TODO Rpc Exceptions");
+                throw resp.Error.Code switch
+                {
+                    ParseError => new ParseError(),
+                    InvalidRequest => new InvalidRequest(),
+                    MethodNotFound => new MethodNotFound(),
+                    InvalidParams => new InvalidParams(),
+                    InternalError => new InternalError(),
+                    _ => new ServerError(resp.Error.Message)
+                };
             }
 
             if (resp != null)
@@ -24,7 +39,7 @@ namespace JsonRpc2.Clients
                 return resp.Result;
             }
 
-            throw new Exception("TODO Rpc Exceptions");
+            throw new InternalError();
         }
     }
 }
---
+++ JsonRpc2/Exceptions/RpcExceptions.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace JsonRpc2.Exceptions
+{
+    public class RpcError : Exception
+    {
+        protected RpcError(string message) : base(message)
+        {
+        }
+    }
+
+    public class ParseError : RpcError
+    {
+        public ParseError() : base("Parse error")
+        {
+        }
+    }
+
+    public class InvalidRequest : RpcError
+    {
+        public InvalidRequest() : base("Invalid Request")
+        {
+        }
+    }
+
+    public class MethodNotFound : RpcError
+    {
+        public MethodNotFound() : base("Method not found")
+        {
+        }
+    }
+
+    public class InvalidParams : RpcError
+    {
+        public InvalidParams() : base("Invalid params")
+        {
+        }
+    }
+
+    public class InternalError : RpcError
+    {
+        public InternalError() : base("Internal error")
+        {
+        }
+    }
+
+    public class ServerError : RpcError
+    {
+        public ServerError(string message) : base(message)
+        {
+        }
+    }
+}
