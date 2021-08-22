201b3b35489ea3b2f6db114cd951fbe53e7a7ef5
Matthew Burkard <matthewburkard@protonmail.com>
2021-08-21 22:17:24 -0400

    feat: add test project
    Add project for unit tests.

--- Src/ClientTests/UnitTest1.cs
+++ Src/ClientTests/UnitTest1.cs
@@ -1,18 +1,38 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using JsonRpcClient.Clients;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace ClientTests
 {
     public class Tests
     {
+        private TestClient _client;
+
         [SetUp]
         public void Setup()
         {
+            _client = new TestClient("http://localhost:8000/api/v1");
         }
 
         [Test]
-        public void Test1()
+        public async Task Test1()
         {
-            Assert.Pass();
+            Assert.AreEqual(await _client.GetStrings(), new List<string>());
+        }
+    }
+
+    public class TestClient : RpcHttpClient
+    {
+        public TestClient(string baseUri) : base(baseUri)
+        {
+        }
+
+        public async Task<List<string>> GetStrings()
+        {
+            var v = await Call("get_props");
+            return JsonConvert.DeserializeObject<List<string>>(v.ToString()!);
         }
     }
 }
